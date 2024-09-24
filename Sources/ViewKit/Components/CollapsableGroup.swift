8dbdf72e19bd231a84dfe710d49946cd3cabc341
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2024-09-24 20:47:56 +0300

    doc: collapsable group init

--- Sources/ViewKit/Components/CollapsableGroup.swift
+++ Sources/ViewKit/Components/CollapsableGroup.swift
@@ -23,6 +23,34 @@ public struct CollapsableGroup<Header: View, Content: View>: View {
     )
     
     // MARK: - Life Cycle
+    ///
+    /// Creates a `CollapsableGroup` with a header and content view, providing optional customizations.
+    ///
+    /// - Parameters:
+    ///   - isExpanded: A binding to a Boolean value that determines whether the group is expanded or collapsed. When `true`, the content is visible, otherwise, it is hidden.
+    ///   - isHeaderTappable: A Boolean value that indicates whether tapping on the header should toggle the collapsed/expanded state. The default is `true`.
+    ///   - spacing: A `CGFloat` value representing the vertical spacing between the header and the content. The default is `8`.
+    ///   - header: A `ViewBuilder` that constructs the view to be used as the header of the collapsable group.
+    ///   - content: A `ViewBuilder` that constructs the view to be used as the content of the collapsable group, which will be shown or hidden based on the `isExpanded` state.
+    ///
+    /// The `CollapsableGroup` offers a collapsible view group, with the ability to customize its header, content, and spacing, and provides the option for users to tap the header to toggle the expanded state. The `header` and `content` views are passed as closures and the expanded state is controlled externally through the `isExpanded` binding.
+    ///
+    /// ## Example:
+    /// ```swift
+    /// @State private var isExpanded = false
+    ///
+    /// var body: some View {
+    ///     CollapsableGroup(isExpanded: $isExpanded) {
+    ///         HStack {
+    ///             Text("Header")
+    ///             Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
+    ///         }
+    ///     } content: {
+    ///         Text("Content")
+    ///     }
+    /// }
+    /// ```
+    ///
     public init(
         isExpanded: Binding<Bool>,
         isHeaderTappable: Bool = true,
